5ccc88654e4e3204740a1f03322fd4461d796dda
camillevacheret2010-hash <camille.vacheret2010@gmail.com>
2026-07-29 09:37:49 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -3654,6 +3654,24 @@ async def export_json(interaction):
             ephemeral=True
         )
 
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=10000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
 
 
+
+
+keep_alive()
 bot.run(os.getenv('DISCORD_TOKEN'))
